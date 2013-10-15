2265ca13cc50011ff18cd567f65f51e3a0783f10
Gareth Owen <drgowen@gmail.com>
2013-10-15 19:33:47 +0100

    first sector decrypt plus tidy up

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 pytruecrypt
 ===========
   
-Truecrypt volume passing library by Gareth Owen, University of Portsmouth  
+Truecrypt volume parsing library by Gareth Owen, University of Portsmouth  
   
-Current status: Dumps decrypted header - Can dump any decrypted sector if you take keys from header - see code for example.  
-  
-TODO: Restructure into library, header passing, add more options  
+Current status: 
+- Dumps decrypted header
+- Decodes header fields
+- Dumps decrypted first sector
+- Easy to use - just call decrypt_sector()
+
+TODO: 
+- Restructure into library
+- add more options  
   
 Example usage:  
   
@@ -13,7 +19,7 @@ pytruecrypt.py file password
   
 Example:  
 
-	[gho@gho-ubook pytruecrypt]$ python2.7 pytruecrypt.py ../test.tc abc123 | hd  
+	[gho@gho-ubook pytruecrypt]$ python2.7 pytruecrypt.py ../test.tc abc123   
 	000000: 54 52 55 45 00 05 07 00  43 70 7f 07 00 00 00 00   TRUE....Cp......  
 	000010: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00   ................  
 	000020: 00 00 00 00 00 00 00 00  00 4c 00 00 00 00 00 00   .........L......  
@@ -38,3 +44,4 @@ And using struct to pass the fields:
 	CRC3=2168932094,  
 	Keys='f.\x96\tW\xbb~\x82\xf8\xfc\x82{U\x84\xec\xac\xdb\x1f\xa9d|\xafF\xd5&\x9a\xee8\xb9S\x7f&0\xa1u\x0cV\xc9-\xc6~h\t)\t\x80\xb0\xd7\xca\xc8\xc8\xc8\x0eo\x8d\xbd\x8d\x80\xcd"\xd0\xa6\x8c\x99\xdb\xd6\xca\xc1R\x00,\x97\xedq\xa9\x83}!Bd\xf9/\t\x84\xbd\x05TU\x1d\xa1\xc3\xb1&\rk\\~\xec\x14\x0f\x19\xbb\x8bi\x8a~\xa2M$\xfe\xf5\xb8y\xf6\xa1\xe5\x15\xa9\xaai\xe7l\xec_:\xdce\x94\'e"=L|\x0b\xc3\x01\xfb\x14\x05\x14\xd8\x15v\x10t\x1d\x9f\xc6\x97\x8fY\xf2jT(\xaa\x13qo\x8f\xab\xd4\t\xa1\xc1\xa5<\n\n\xd58\x07\xcfdH\x9b+\xc4^\xcfO\xe6\xa5wbZt#\xed\xefI\x0cx\x9f\x08\xb7\x89\xee\xc4\xa0\xc3\x7fe\xb9\x92jDS@\xea\x8bk\x043\x0b\xdc!\x95\x9a\xd7\xd4\x0b\x07\xeaS\xa8)\xf2\x1cY$%\xff\xc7\'s\xd0\x9f%\xf2\xa1C\x90\x92/\x14z]1\xb2\xfaY\xbb+e')  
 
+First sector also displayed
--- pytruecrypt.py
+++ pytruecrypt.py
@@ -7,6 +7,17 @@ from galois import *
 import binascii
 import struct 
 
+# hexdump print function
+def hexdump(src, length=16):
+    FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
+    lines = []
+    for c in xrange(0, len(src), length):
+        chars = src[c:c+length]
+        hex = ' '.join(["%02x" % ord(x) for x in chars])
+        printable = ''.join(["%s" % ((ord(x) <= 127 and FILTER[ord(x)]) or '.') for x in chars])
+        lines.append("%04x  %-*s  %s\n" % (c, length*3, hex, printable))
+    return ''.join(lines)
+
 # Integer to little endian (int array)
 def inttoLE(x):
 	str=[]
@@ -32,6 +43,7 @@ def strtobuf(x):
 # XOR two strings
 def xor(a,b):
 	return ''.join([chr(ord(a[i])^ord(b[i])) for i in range(len(a))])
+
 
 # Generate sector ek2(n)
 def getXTSek2n(_aes, sector):
@@ -61,13 +73,16 @@ def decrypt_sector(aes, aesxts, sector, ciphertext, offset=0):
 		tc_plain += ptext
 	return tc_plain
 
+################## CODE START ######################
+
 # Usage
 if len(sys.argv) != 3:
 	print "pytruecrypt.py filename password"
 	sys.exit(0)
 	
-# Open file
-tchdr = open(sys.argv[1], "rb").read(131072)
+# Open file and read header
+inf = open(sys.argv[1], "rb")
+tchdr = inf.read(131072)
 
 # First 64 bytes are salt
 salt = tchdr[0:64]
@@ -77,17 +92,18 @@ salt = tchdr[0:64]
 pwhash= PBKDF2(sys.argv[2], salt, 64, count=2000, prf=lambda p,s: HMAC.new(p,s,RIPEMD).digest())
 aeskey = pwhash[0:32]
 xtskey = pwhash[32:64]
-#print "Hdr keys:", binascii.hexlify(aeskey+xtskey)
+print "Hdr keys:", binascii.hexlify(aeskey+xtskey)
 
 # Load hdr keys into pycrypto
 aes = AES.new(aeskey, AES.MODE_ECB)
 aesxts = AES.new(xtskey, AES.MODE_ECB)
 
 # decrypt header
+print "Plaintext header"
 tchdr_plain = decrypt_sector(aes, aesxts, 0, tchdr, 64)
 
 # dump header to screen
-sys.stdout.write(tchdr_plain)
+print hexdump(tchdr_plain)
 
 # Dump rest of header - normally random data
 #for i in range(1, 256):
@@ -95,10 +111,26 @@ sys.stdout.write(tchdr_plain)
 #	sys.stdout.write(tc_plain)
 
 # Parse first few fields of header
+
+# Parse header using python's struct and print out fields
+print "Parsed header"
 TCHDR = namedtuple('TCHDR', "Magic HdrVersion MinProgVer CRC Reserved HiddenVolSize VolSize DataStart DataSize Flags SectorSize Reserved2 CRC3 Keys")
 hdr_decoded = struct.unpack(">4sH", tchdr_plain[0:6]) + struct.unpack("<H", tchdr_plain[6:8]) + struct.unpack(">I16sQQQQII120sI256s", tchdr_plain[8:448])
 print
-print TCHDR._make(hdr_decoded)
 
+hdrstruct = TCHDR._make(hdr_decoded)
 
+print hdrstruct
+
+# Print Primary and Secondary keys for AES-XTS of main data
+print "KEYS:", binascii.hexlify(hdrstruct.Keys[0:64])
+
+# Load primary and secondary key and decrypt first sector
+aes = AES.new(hdrstruct.Keys[0:32], AES.MODE_ECB)
+aesxts = AES.new(hdrstruct.Keys[32:64], AES.MODE_ECB)
+
+print
+print "FIRST SECTOR"
+# note IV for first sector = actual sector number (not minus header) = 256
+print hexdump(decrypt_sector(aes, aesxts, 256, inf.read(512), 0))
 
