be70fb4e347bebf9d0f12a33a1260b12a8f45fed
Dr Gareth Owen <drgowen@gmail.com>
2013-10-19 14:20:44 +0100

    added licence header

--- examples/pwcracker.py
+++ examples/pwcracker.py
@@ -1,6 +1,19 @@
-# Truecrypt parsing library for Python by Gareth Owen
+# PyTruecrypt parsing library for Python by Gareth Owen
 # https://github.com/drgowen/pytruecrypt/
 # See LICENCE for licence details
+
+# PyTruecrypt is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PyTruecrypt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PyTruecrypt.  If not, see <http://www.gnu.org/licenses/>.
 
 # Truecrypt password cracker
 
